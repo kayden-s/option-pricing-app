adee33bcfa41375f80dc54982b4fc58aa5b33030
kayden-s <kayden.spamm@gmail.com>
2025-10-01 23:51:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,8 +133,6 @@ if pricing_method == OPTION_PRICING_MODEL.BLACK_SCHOLES.value:
                 st.error("Unable to proceed with calculations due to data fetching error.")
         except Exception as e:
             st.error(f"Error during calculation: {str(e)}")
-    else:
-        st.info("Click 'Calculate option price' to see results.")
 
 elif pricing_method == OPTION_PRICING_MODEL.MONTE_CARLO.value:
     # Parameters for Monte Carlo simulation
